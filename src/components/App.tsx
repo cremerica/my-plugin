8ce319afe120b8831f0736bd27a2af82ff48256d
cremerfc <cremerfc@users.noreply.github.com>
2023-07-25 10:20:22 -0400

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,14 +8,14 @@ import {
 import "../baseStyles.css";
 import ErrorBoundary from "./ErrorBoundary";
 import PluginContext from "./PluginContext";
+import App from "./snow";
 
 const App: React.FC = () => {
   return (
     <ErrorBoundary>
       <PluginProvider>
         <Stack>
-          <Logo />
-          <Title level={1}>My Awesome Cortex Plugin</Title>
+          <SimpleTable config={App.config} items={App.posts} />
         </Stack>
         <PluginContext />
       </PluginProvider>
